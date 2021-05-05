0ed4c80e0d2182924a1ec3175a5033dd7d49b442
stanleyluuuu <stanley860920@gmail.com>
2021-05-05 15:26:29 +0800

    HW2

---
+++ Homework2/hw2_template.py
@@ -0,0 +1,63 @@
+'''
+NTHU EE Machine Learning HW2
+Author: 
+Student ID: 
+'''
+import numpy as np
+import pandas as pd
+import math
+import scipy.stats
+import argparse
+import pdb
+
+
+# do not change the name of this function
+def BLR(train_data, test_data_feature, O1=5, O2=5):  # remember to set best choice O1 and O2 as default
+    '''
+    output: ndarray with size (length of test_data, )
+    '''
+    mean, var = np.array(train_data).mean(axis=0), np.array(train_data).var(axis=0)
+    pdb.set_trace()
+
+    return y_BLRprediction 
+
+
+# do not change the name of this function
+def MLR(train_data, test_data_feature, O1=5, O2=5):  # remember to set best choice O1 and O2 as default
+    '''
+    output: ndarray with size (length of test_data, )
+    '''
+
+
+    return y_MLLSprediction 
+
+
+def CalMSE(data, prediction):
+
+    squared_error = (data - prediction) ** 2
+    sum_squared_error = np.sum(squared_error)
+    mean__squared_error = sum_squared_error/prediction.shape[0]
+
+    return mean__squared_error
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-O1', '--O_1', type=int, default=5)
+    parser.add_argument('-O2', '--O_2', type=int, default=5)
+    args = parser.parse_args()
+    O_1 = args.O_1
+    O_2 = args.O_2
+    
+    data_train = pd.read_csv('Training_set.csv', header=None).to_numpy()
+    data_test = pd.read_csv('Validation_set.csv', header=None).to_numpy()
+    data_test_feature = data_test[:, :3]
+    data_test_label = data_test[:, 3]
+    predict_BLR = BLR(data_train, data_test_feature, O1=O_1, O2=O_2)
+    predict_MLR = MLR(data_train, data_test_feature, O1=O_1, O2=O_2)
+
+    print('MSE of BLR = {e1}, MSE of MLR= {e2}.'.format(e1=CalMSE(predict_BLR, data_test_label), e2=CalMSE(predict_MLR, data_test_label)))
+
+
+if __name__ == '__main__':
+    main()
