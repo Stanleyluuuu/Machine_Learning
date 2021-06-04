b61aff676f2010e728a8f6aeb11e01168d92a523
stanleyluuuu <stanley860920@gmail.com>
2021-06-04 22:48:43 +0800

    submit some results

--- Final/final_main.py
+++ Final/final_main.py
@@ -1,31 +1,18 @@
 import argparse
 import numpy as np
-import csv
 import pdb
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from final_utils import *
+import wandb
 
-def get_data():
-    train_dir, test_dir = './fgd_data/train.csv', './fgd_data/test.csv'
-    train_x, train_y, test= [], np.zeros((20621, 5)), []
-    with open(train_dir) as d:
-        rows = csv.reader(d)
-        for i, row in enumerate(rows):
-            if i > 0:
-                row = [float(a) for a in row[2:]]
-                train_x.append(row[:-1])
-                train_y[i-1][int(row[-1]) - 1] = 1
-    with open(test_dir) as d:
-        rows = csv.reader(d)
-        for i, row in enumerate(rows):
-            if i > 0:
-                row = [float(a) for a in row[2:]]
-                test.append(row)
-
-    return train_x, train_y, test
-
+            
 # there are 20621 training datas, 500 test datas
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Machine Learning Fianl Project')
@@ -33,65 +20,158 @@ if __name__ == "__main__":
                         help='Input the method you want to use, e.g. LR(Linear Regression), KNN(K-Nearest Neighbor), DT(Decesion Tree), RF(Random Forest) and CNN(Convolutional Neural Network)(default: CNN)')
     parser.add_argument('--neighbor', type=int, default=3, metavar='N',
                         help='Input number of neighbor (default: 3)')
-    parser.add_argument('--depth', type=int, default=1, metavar='D',
-                        help='Input max depth for Random Forest (default: 1)')
-    parser.add_argument('--state', type=int, default=1, metavar='S',
-                        help='Input random state for Random Forest (default: 1)')
-    # parser.add_argument('--batchsize', type=int, default=100, metavar='N',
-    #                     help='Input batch size for training (default: 100)')
-    # parser.add_argument('--epoch', type=int, default=200, metavar='N',
-    #                     help='Input number of epoch for training (default: 200)')
-    # parser.add_argument('--optimizer', type=str, default='Adam', metavar='o',
-    #                     help='Input the optimizer, Adam or SGD (default: Adam)')
-    # parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
-    #                     help='Input the learning rate of the optimizer (default: 0.001)')
-    # parser.add_argument('--train', action='store_true', default=False,
-    #                     help='Use training mode')
-    # parser.add_argument('--evaluate', action='store_true', default=False,
-    #                     help='Use validation mode')
-    # parser.add_argument('--test', action='store_true', default=False,
-    #                     help='Use testing mode')
-    train_x, train_y, test = get_data()
+    parser.add_argument('--depth', type=int, default=20, metavar='D',
+                        help='Input max depth for Random Forest (default: 20)')
+    parser.add_argument('--state', type=int, default=15, metavar='S',
+                        help='Input random state for Random Forest (default: 15)')
+    parser.add_argument('--batchsize', type=int, default=100, metavar='N',
+                        help='Input batch size for training (default: 100)')
+    parser.add_argument('--epoch', type=int, default=200, metavar='N',
+                        help='Input number of epoch for training (default: 200)')
+    parser.add_argument('--optimizer', type=str, default='Adam', metavar='o',
+                        help='Input the optimizer, Adam or SGD (default: Adam)')
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                        help='Input the learning rate of the optimizer (default: 0.001)')
+    parser.add_argument('--train', action='store_true', default=False,
+                        help='Use training mode')
+    parser.add_argument('--test', action='store_true', default=False,
+                        help='Use testing mode')
+    parser.add_argument('--find', action='store_true', default=False,
+                        help='Use finding mode')
     args = parser.parse_args()
+    
+    
     if args.method == "LR":
+        train_x, train_y, test = get_data(args)
         print("Using Linear Regression")
         model = LinearRegression().fit(train_x, train_y)
-        prediction = model.predict(train_x)
-        acc = sum(np.argmax(prediction, axis=1) == np.argmax(train_y, axis=1)) / len(train_x)
-        print("Accuracy on training set =", acc)
-        # prediction = model.predict(test)
-        # pdb.set_trace()
-        # print(prediction)
+        p_train = model.predict(train_x)
+        soft_pred = softmax(p_train)
+        acc = sum(np.argmax(soft_pred, axis=1) == np.argmax(train_y, axis=1)) / len(train_x)
+        loss = get_loss(soft_pred, train_y)
+        print("Accuracy on training set = {e1:.3f}, loss = {e2:.3f}".format(e1=acc, e2=loss))
+        prediction = model.predict(test)
+        soft_pp = softmax(prediction)
+        save(soft_pp)
 
     if args.method == "KNN":
+        train_x, train_y, test = get_data(args)
         print("Using K-Nearest Neighbor")
         model = KNeighborsClassifier(n_neighbors=args.neighbor).fit(train_x, train_y)
-        prediction = model.predict(train_x)
-        acc = sum(np.argmax(prediction, axis=1) == np.argmax(train_y, axis=1)) / len(train_x)
-        print("Accuracy on training set =", acc)
-        # pdb.set_trace()
-        # prediction = model.predict(test)
-        # print(prediction)
+        p_train = model.predict(train_x)
+        acc = sum(np.argmax(p_train, axis=1) == np.argmax(train_y, axis=1)) / len(train_x)
+        loss = get_loss(p_train, train_y, epsilon=1e-5)
+        print("Accuracy on training set = {e1:.3f}, loss = {e2:.3f}".format(e1=acc, e2=loss))
+        prediction = model.predict(test)
+        save(prediction)
 
     if args.method == "DT":
+        train_x, train_y, test = get_data(args)
         print("Using Decision Tree")
         model = DecisionTreeClassifier().fit(train_x, train_y)
-        prediction = model.predict(train_x)
-        acc = sum(np.argmax(prediction, axis=1) == np.argmax(train_y, axis=1)) / len(train_x)
-        print("Accuracy on training set =", acc)
-        # pdb.set_trace()
-        # prediction = model.predict(test)
-        # print(prediction)
+        p_train = model.predict(train_x)
+        acc = sum(np.argmax(p_train, axis=1) == np.argmax(train_y, axis=1)) / len(train_x)
+        loss = get_loss(p_train, train_y, epsilon=1e-5)
+        print("Accuracy on training set = {e1:.3f}, loss = {e2:.3f}".format(e1=acc, e2=loss))
+        prediction = model.predict(test)
+        save(prediction)
 
     if args.method == "RF":
+        train_x, train_y, test = get_data(args)
         print("Using Random Forest")
         model = RandomForestClassifier(max_depth=args.depth, random_state=args.state).fit(train_x, train_y)
-        prediction = model.predict(train_x)
-        acc = sum(np.argmax(prediction, axis=1) == np.argmax(train_y, axis=1)) / len(train_x)
-        print("Accuracy on training set =", acc)
-        pdb.set_trace()
-        # prediction = model.predict(test)
-        # print(prediction)
+        p_train = model.predict(train_x)
+        acc = sum(np.argmax(p_train, axis=1) == np.argmax(train_y, axis=1)) / len(train_x)
+        loss = get_loss(p_train, train_y, epsilon=1e-5)
+        print("Accuracy on training set = {e1:.3f}, loss = {e2:.3f}".format(e1=acc, e2=loss))
+        prediction = model.predict(test)
+        save(prediction)
 
     if args.method == "CNN":
-        print("Using Convolution Neural Network")
+        trainloader, testloader = get_data(args, row=1)
+        if args.train:
+            sweep_config = {'method': 'random', #grid, random
+                            'metric': {'name': 'loss',
+                                    'goal': 'minimize'},
+                            'parameters': {'epochs': {'values': [args.epoch]},
+                                        'batch_size': {'values': [args.batchsize]},
+                                        'learning_rate': {'values': [args.lr]},
+                                        'optimizer': {'values': ['adam', 'sgd']}}}
+            sweep_id = wandb.sweep(sweep_config, project="Machine Learning Final Project")
+            config_defaults = {'epochs': args.epoch,
+                            'batch_size': args.batchsize,
+                            'learning_rate': args.lr,
+                            'optimizer': args.optimizer}
+            wandb.init(config=config_defaults)
+            config = wandb.config
+            print("Training the Convolution Neural Network model")
+            model = CNN()
+            if args.optimizer == "SGD":
+                optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
+            elif args.optimizer == "Adam":
+                optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.999))
+            criterion = nn.CrossEntropyLoss()
+            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+            model.to(device)
+            print("Start training process")
+            for epoch in range(args.epoch):
+                model.train()
+                total, epoch_correct, epoch_loss = 0, 0, 0
+                for data in trainloader:
+                    inputs, labels = data[0].to(device), data[1].to(device)
+                    optimizer.zero_grad()
+                    outputs = model(inputs)
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    epoch_correct += (predicted == labels).sum().item()
+                    loss = criterion(outputs, labels)
+                    loss.backward()
+                    optimizer.step()
+                    epoch_loss += loss.item()
+                    wandb.log({"Training batch loss":loss.item() / labels.size(0)})
+                    wandb.log({"Training batch accuracy":(predicted == labels).sum().item() / labels.size(0) * 100})
+                wandb.log({"Training loss per epoch":epoch_loss / total})
+                wandb.log({"Training accuracy per epoch":epoch_correct / total * 100})
+                # saving check point
+                PATH = "./checkpoint/epoch" + str(epoch + 1)
+                torch.save(model.state_dict(), PATH)
+
+        if args.find:
+            # print("Using Convolution Neural Network model")
+            model = CNN()
+            criterion = nn.CrossEntropyLoss()
+            result = []
+            for epoch in range(args.epoch):
+                PATH = "./checkpoint/epoch" + str(epoch + 1)
+                model.load_state_dict(torch.load(PATH))
+                device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+                model.to(device)
+                model.eval()
+                with torch.no_grad():
+                    total, epoch_correct, epoch_loss = 0, 0, 0
+                    for data in trainloader:
+                        inputs, labels = data[0].to(device), data[1].to(device)
+                        outputs = model(inputs)
+                        _, predicted = torch.max(outputs.data, 1)
+                        total += labels.size(0)
+                        epoch_correct += (predicted == labels).sum().item()
+                        loss = criterion(outputs, labels)
+                        epoch_loss += loss.item()
+                result.append((epoch+1, epoch_loss / total, epoch_correct / total * 100))
+            a = min(result, key=lambda x: x[1])
+            print("The lowest loss =", a)
+            b = max(result, key=lambda x: x[2])
+            print("The highest accuracy =", b)
+        if args.test:
+            print("Using Convolution Neural Network model")
+            model = CNN()
+            PATH = "./checkpoint/epoch" + str(189)
+            model.load_state_dict(torch.load(PATH))
+            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+            model.to(device)
+            model.eval()
+            with torch.no_grad():
+                for data in testloader:
+                    inputs= data[0].to(device)
+                    outputs = model(inputs)
+                save(softmax(np.array(outputs.cpu())))
---
+++ Final/final_utils.py
@@ -0,0 +1,123 @@
+import numpy as np
+import csv
+import torch
+import pdb
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+import wandb
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv1d(1, 6, 3, stride=1)
+        self.conv2 = nn.Conv1d(6, 12, 3, stride=1)
+        self.conv3 = nn.Conv1d(12, 12, 3, stride=1)
+        self.conv4 = nn.Conv1d(12, 6, 3, stride=1)
+        # self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(p=0.5)
+        self.bn1 = nn.BatchNorm1d(6)
+        self.bn2 = nn.BatchNorm1d(12)
+        self.fc1 = nn.Linear(6 * 16,  100)
+        self.fc2 = nn.Linear(100, 80)
+        self.fc3 = nn.Linear(80, 5)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.bn2(F.relu(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        x = self.bn1(F.relu(self.conv4(x)))
+        x = x.view(-1, 6 * 16)
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = self.fc3(x)
+
+        return x
+
+def read_file(data_dir, label=0):
+    data = []
+    if label == 1:
+        labels = []
+    elif label == 5:
+        labels = np.zeros((20621, 5))
+
+    with open(data_dir) as d:
+        rows = csv.reader(d)
+        for i, row in enumerate(rows):
+            if i > 0:
+                row = [float(a) for a in row[2:]]
+                if label != 0:
+                    data.append(row[:-1])
+                else:
+                    data.append(row)
+                if label == 1:
+                    labels.append(int(row[-1]) - 1)
+                elif label == 5:
+                    labels[i-1][int(row[-1]) - 1] = 1
+    if label == 0:
+        return data
+    else:
+        return data, labels
+
+
+def get_data(args, row=5): # get the datas from csv file and turns into the format I need
+    train_dir, test_dir = './fgd_data/train.csv', './fgd_data/test.csv'
+    train_x, train_y = read_file(train_dir, row)
+    test = read_file(test_dir)
+    if args.method == "CNN":
+        train_x, test = np.array(train_x), np.array(test)
+        ex_tx, ex_t = train_x[:, np.newaxis, :], test[:, np.newaxis, :]
+        preprocess = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=(0.5), std=(0.5))])
+        t_train_x, t_train_y, t_test = np.einsum('abc->bca', preprocess(ex_tx)), torch.tensor(train_y, dtype=torch.long), np.einsum('abc->bca', preprocess(ex_t))
+        trainset = torch.utils.data.TensorDataset(torch.Tensor(t_train_x), t_train_y)
+        testset = torch.utils.data.TensorDataset(torch.Tensor(t_test))
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batchsize, shuffle=True)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=500, shuffle=False)
+
+        return trainloader, testloader
+    else:
+        
+        return np.array(train_x), np.array(train_y), np.array(test)
+
+
+def softmax(prediction): # softmax function
+    a = np.exp(prediction)
+    b = a.sum(axis=1)
+
+    return a / b[:, np.newaxis]
+
+def get_loss(prediction, y, epsilon=None): # calculate cross entropy loss, adding epsilon to make sure no log(0) won't appear
+    if epsilon:
+        log_loss = (1 / prediction.shape[0]) * np.nansum(y * np.log(prediction + epsilon))
+    else:
+        log_loss = (1 / prediction.shape[0]) * np.nansum(y * np.log(prediction))
+    
+    return -log_loss
+
+def save(prediction): # save the result as test.csv file in the format of AIdea
+    with open("test.csv", 'w') as f:
+        f.write("ID,C1,C2,C3,C4,C5")
+        f.write("\n")
+        for i, p in enumerate(prediction):
+            f.write(str(i+1))
+            for n in p:
+                f.write("," + str(n))
+            if i != 499:
+                f.write("\n")
+
+def db(args):
+    sweep_config = {'method': 'random', #grid, random
+                    'metric': {'name': 'loss','goal': 'minimize'},
+                    'parameters': {'epochs': {'values': [args.epoch]},
+                                   'batch_size': {'values': [args.batchsize]},
+                                   'learning_rate': {'values': [args.lr]},
+                                   'optimizer': {'values': ['adam', 'sgd']}}}
+    sweep_id = wandb.sweep(sweep_config, project="Machine Learning Final Project")
+    config_defaults = {'epochs': args.epoch,
+                       'batch_size': args.batchsize,
+                       'learning_rate': args.lr,
+                       'optimizer': args.optimizer}
+    wandb.init(config=config_defaults)
+    config = wandb.config
+
+    return config
